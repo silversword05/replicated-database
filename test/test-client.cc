073f4d5cc0942436c9d207ebbfb17f47c85e6bfb
mpatel48 <mpatel48@node0.p2distributed.advosuwmadison-pg0.utah.cloudlab.us>
2023-04-05 16:32:19 -0600

    Test corrected

--- test/test-client.cc
+++ test/test-client.cc
@@ -14,19 +14,24 @@ int main(int argc, char *argv[]) {
   ClientService service(clientId);
 
   uint reqs = 10;
+
+  std::unordered_set<uint> reqNumSet;
   // Do some put operations
   for (uint i = 0; i < reqs; i++) {
-    service.put(i, i + 100);
+    auto val = service.put(i, i + 100);
+    if (val.has_value()) {
+      reqNumSet.insert(val.value());
+    }
   }
 
   utils::print("Send some put requests!!");
 
   // check if put done
-  for (uint i = 0; i < reqs; i++) {
+  for (auto elem : reqNumSet) {
     while (true) {
-      auto check = service.checkPutDone(i);
+      auto check = service.checkPutDone(elem);
       if (check.has_value()) {
-        utils::print("Request No: ", i, " has result: ", check.value());
+        utils::print("Request No: ", elem, " has result: ", check.value());
         break;
       }
     }
